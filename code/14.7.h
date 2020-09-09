e78a3207d7dd45bc270e2827cdc38dc2e390dadf
tiandada <tianqiank@163.com>
2020-09-09 15:26:00 +0800

    add 14.2

---
+++ code/14.7.h
@@ -0,0 +1,29 @@
+#ifndef _13_44_H
+#define _13_44_H
+
+#include <iostream>
+#include <memory>
+#include <algorithm>
+
+class String {
+public:
+	String() : String("") { }
+	String(const char *);
+	String(const String&);
+	String& operator=(const String&);
+	~String();
+
+	const char *c_str() const { return elements; }
+	size_t size() const { return end - elements; }
+	size_t length() const { return end - elements - 1; }
+private:
+	std::pair<char*, char*> alloc_n_copy(const char*, const char*);
+	void range_initializer(const char*, const char*);
+	void free();
+private:
+	char *elements;
+	char *end;
+	std::allocator<char> alloc;
+};
+
+#endif
