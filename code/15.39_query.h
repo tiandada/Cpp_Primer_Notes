e55ed20ed669da2fc44d43e0444aace09334abf3
tiandada <tianqiank@163.com>
2020-09-16 17:29:58 +0800

    add 15.9

---
+++ code/15.39_query.h
@@ -0,0 +1,12 @@
+#ifndef _15_39_QUERY_H
+#define _15_39_QUERY_H
+#include "15.35_text_query.h"
+#include "15.35_query_base.h"
+#include "15.35_word_query.h"
+#include "15.35_query.h"
+#include "15.35_Binary_query.h"
+#include "15.35_orquery.h"
+#include "15.35_andquery.h"
+#include "15.35_notquery.h"
+
+#endif
