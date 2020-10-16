f0fe8ae406e35e777a9cfade5ae981686dc19b94
tiandada <tianqiank@163.com>
2020-10-16 17:20:36 +0800

    add 19.2

--- code/19.10.cpp
+++ code/19.10.cpp
@@ -12,9 +12,9 @@ int main()
 {
 	A *pa = new C;
 	std::cout << typeid(pa).name() << std::endl;
-	//C cobj;
-	//A& ra = cobj;
-	//std::cout << typeid(&ra).name() << std::endl;
+	C cobj;
+	A& ra1 = cobj;
+	std::cout << typeid(&ra1).name() << std::endl;
 	B *px = new B;
 	A& ra = *px;
 	std::cout << typeid(ra).name() << std::endl;
