ec112deec37d2d5976f7e1c27cbb9fa1a7395f0d
tiandada <tianqiank@163.com>
2020-10-16 16:20:03 +0800

    add 19.2

---
+++ code/19.10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <typeinfo>
+
+class A {
+public:
+	virtual ~A() { }
+};
+class B : public A { };
+class C : public B { };
+
+int main()
+{
+	A *pa = new C;
+	std::cout << typeid(pa).name() << std::endl;
+	//C cobj;
+	//A& ra = cobj;
+	//std::cout << typeid(&ra).name() << std::endl;
+	B *px = new B;
+	A& ra = *px;
+	std::cout << typeid(ra).name() << std::endl;
+	return 0;
+}
